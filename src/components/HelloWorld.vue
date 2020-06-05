19920406ba359ef9efbe16f86846a1097d820d75
Masaya Kazama <miyan@t-p.jp>
2020-06-06 03:59:57 +0900

    WIP

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,143 @@
 <template>
   <div>
-    <h1>hello</h1>
+    <h1>#金曜GUI</h1>
+    <h2>ありよりのなしレンジスライダーやりたい</h2>
+
+    <section class="control">
+      <label>
+        <div class="range">
+          <p><input v-model="left" type="text" /></p>
+          <input
+            v-model.number="value"
+            type="range"
+            class="value"
+            min="-1"
+            max="1"
+            step="any"
+          />
+          <p><input v-model="right" type="text" /></p>
+        </div>
+        <div class="depth">
+          深み
+          <input v-model.number="depth" type="range" min="2" max="6" step="1" />
+          {{ depth }}
+        </div>
+
+        <div class="values">
+          <!-- <p>
+            {{ value.toFixed(2) }}
+          </p>
+          <p>
+            {{ binary }}
+          </p> -->
+          <h2>
+            {{ joinedValue }}
+          </h2>
+          <!-- <div class="result">
+            <span
+              v-for="(item, i) in computedValueList"
+              :key="i"
+              :style="getStyle(i)"
+            >
+              {{ item }}
+            </span>
+          </div> -->
+        </div>
+      </label>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  data() {
+    return {
+      value: 0,
+      left: 'あり',
+      right: '無し',
+      depth: 4,
+    }
+  },
+  computed: {
+    binary(): string {
+      return Math.floor(
+        Math.abs(this.value) * parseInt('1'.repeat(this.depth), 2)
+      ).toString(2)
+    },
+    computedValueList(): string[] {
+      if (this.binary === '0') {
+        return [[this.left, this.right, ''].join('か')]
+      }
+      const isNegative = this.value < 0
+      const map: Record<string, string> = isNegative
+        ? {
+            '0': this.right,
+            '1': this.left,
+          }
+        : {
+            '0': this.left,
+            '1': this.right,
+          }
+      const itemList = this.binary.split('').map((item) => map[item])
+      return itemList.length === 1 ? [itemList[0] + 'かも'] : itemList
+    },
+    joinedValue(): string {
+      let contCount = 0
+
+      return this.computedValueList.reduce((acc, item, i, ary) => {
+        const prev = ary[i - 1]
+        const isCont = prev === item
+        if (isCont && contCount === 0) {
+          contCount++
+        } else {
+          contCount = 0
+        }
+
+        return (
+          acc +
+          (!prev || contCount === 1 ? '' : 'の') +
+          (i + 1 === ary.length ? item : item.slice(0, 2))
+        )
+      }, '')
+    },
+  },
+  methods: {
+    getStyle(index: number): Record<string, string> {
+      const len = this.computedValueList.length
+
+      return {
+        fontSize: (16 * len) / (index + 1) + 'px',
+      }
+    },
+  },
+})
 </script>
 
 <style scoped lang="scss">
+.value {
+  width: 300px;
+}
+.range,
+.result {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+// .result {
+//   p:nth-child(1) {
+//     font-size: 200%;
+//   }
+//   p:nth-child(2) {
+//     font-size: 150%;
+//   }
+// }
+.values {
+  display: flex;
+  flex-direction: column;
+}
 h3 {
   margin: 40px 0 0;
 }
